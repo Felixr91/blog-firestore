b7c82512c935923e6ac9e1a8318fb72d2c5492e0
Felix <felixr91@gmail.com>
2019-03-22 15:34:54 -0600

    nexteditblogs

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -7,7 +7,7 @@
         <h1>{{blog.name}}</h1>
         <p class="blog-content">{{blog.description}}</p>
       </div>
-      <div class="blog-body col-12">
+      <div class="blog-body">
         <h5>{{blog.body}}</h5>
       </div>
       <div class="col-12" v-if="user.uid == blog.creatorId">
@@ -27,31 +27,6 @@
           </div>
         </div>
       </div>
-      <div class="col-6 bg-secondary border border-dark border-bottom-0 rounded">
-        <h3 class="card-title">Comments</h3>
-        <i class="far fa-plus-square fa-3x text-success margin-bottom" @click="adding = true" v-if="!adding"></i>
-        <i class="far fa-minus-square fa-3x text-danger margin-bottom" @click="adding = false" v-if="adding"></i>
-        <div class="add-comment" v-if="adding">
-          <form @submit.prevent="addComment(comment); comment = {};">
-            <div class="form-group">
-              <label for="body">Comment</label>
-              <input class="form-control" type="text" id="body" v-model="comment.body" maxlength="200">
-            </div>
-            <button class="btn btn-dark margin-bottom" @click="adding = false" type="submit">Add Comment</button>
-          </form>
-        </div>
-        <div class="row" v-for="(comment, index) in comments">
-          <div class="col-12">
-            <div class="card text-white bg-dark margin-bottom" :class="evenOrOdd(index)"
-              style=" min-width: 30rem; max-width: 30rem;">
-              <div class="card-body">
-                <h3 class="card-title">{{comment.userName}}</h3>
-                <p class="card-text hide">{{comment.body}}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   </div>
 </template>
@@ -62,9 +37,7 @@
     name: 'Blog',
     data() {
       return {
-        body: "",
-        comment: {},
-        adding: false
+        body: ""
       }
     },
     mounted() {
@@ -76,9 +49,6 @@
       },
       user() {
         return this.$store.state.user
-      },
-      comments() {
-        return this.$store.state.comments
       }
     },
     methods: {
@@ -89,20 +59,12 @@
         this.$router.push("/dashboard")
       },
       updateBody() {
-        var published = published
-        debugger
-        this.blog.body = body
+        this.blog.body = this.body
         this.$store.dispatch("updateBlog", this.blog)
       },
       updatePublished() {
         this.blog.published = !this.blog.published
         this.$store.dispatch("updateBlog", this.blog)
-      },
-      addComment(comment) {
-        this.$store.dispatch("addComment", comment)
-      },
-      evenOrOdd(index) {
-        return index % 2 ? "float-left" : "float-right"
       }
     },
     components: {}
@@ -113,14 +75,5 @@
 <style scoped>
   .blog-content {
     word-wrap: break-word;
-  }
-
-  .outline {
-    border: solid black 3px;
-    border-radius: 10px;
-  }
-
-  .margin-bottom {
-    margin-bottom: 15px;
   }
 </style>
